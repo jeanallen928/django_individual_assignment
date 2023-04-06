5c1f722b8a04b4013dc4ed63b6ca12c9de50d512
jeanallen928 <123160000+jeanallen928@users.noreply.github.com>
2023-04-07 07:10:23 +0900

    update sign in, sign up
    added error messages
    reference: 파이썬 장고 기초 실무 5주차

--- accounts/views.py
+++ accounts/views.py
@@ -14,17 +14,19 @@ def sign_up_view(request):
         else:
             return render(request, 'accounts/signup.html')
     elif request.method == 'POST':
-        username = request.POST.get('username', None)
-        password = request.POST.get('password', None)
-        password2 = request.POST.get('password2', None)
-        bio = request.POST.get('bio', None)
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        password2 = request.POST.get('password2', '')
+        bio = request.POST.get('bio', '')
 
         if password != password2:
-            return render(request, 'accounts/signup.html')
+            return render(request, 'accounts/signup.html', {'error': '패스워드를 확인 해 주세요!'})
         else:
+            if username == '' or password == '':
+                return render(request, 'accounts/signup.html', {'error': '이름과 패스워드는 필수입니다.'})
             exist_user = get_user_model().objects.filter(username=username)
             if exist_user:
-                return render(request, 'accounts/signup.html')  # 사용자가 존재하기 때문에 사용자를 저장하지 않고 회원가입 페이지를 다시 띄움
+                return render(request, 'accounts/signup.html', {'error': '계정이 존재합니다.'})
             else:
                 UserModel.objects.create_user(username=username, password=password, bio=bio)
                 return redirect('/sign-in')
@@ -32,15 +34,15 @@ def sign_up_view(request):
 
 def sign_in_view(request):
     if request.method == 'POST':
-        username = request.POST.get('username', None)
-        password = request.POST.get('password', None)
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
 
         me = auth.authenticate(request, username=username, password=password)
         if me is not None:
             auth.login(request, me)
             return redirect('/')
         else:
-            return redirect('/sign-in')
+            return render(request, 'accounts/signin.html', {'error': '이름 혹은 패스워드를 확인 해 주세요.'})
     elif request.method == 'GET':
         user = request.user.is_authenticated
         if user:
